5056d3c7387b1be9021b8d07eb05648b1f56b0e5
budhalantara <budhalantara@gmail.com>
2022-12-11 16:54:48 +0800

    remove unused code

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,6 +1,3 @@
-import { ACCESS_TOKEN_COOKIE_KEY } from '$env/static/private';
-import authToken from '$lib/server/utils/authToken';
-
 /** @type {import('./$types').LayoutServerLoad} */
 export function load({ locals }) {
 	return {
